51c3e75bd14e92147f373b19eddbf507cf714e51
Anders Bjerner <abjerner@skybrud.dk>
2018-07-29 18:29:13 +0200

    Added XML documentation to "SpotifyAlbumType" enum properties

--- src/Skybrud.Social.Spotify/Models/Albums/SpotifyAlbumType.cs
+++ src/Skybrud.Social.Spotify/Models/Albums/SpotifyAlbumType.cs
@@ -4,10 +4,22 @@
     /// Enum class representing the type of a Spotify album.
     /// </summary>
     public enum SpotifyAlbumType {
+
+        /// <summary>
+        /// Indicates that the album is a normal album.
+        /// </summary>
         Album,
+
+        /// <summary>
+        /// Indicates that the album represents a single.
+        /// </summary>
         Single,
-        AppearsOn,
+
+        /// <summary>
+        /// Indicates that the album represents a compilation.
+        /// </summary>
         Compilation
+
     }
 
 }
