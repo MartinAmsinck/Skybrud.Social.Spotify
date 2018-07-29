797ec321736131984fe603ec9ac8b1760f0c50c1
Anders Bjerner <abjerner@skybrud.dk>
2018-07-29 22:43:44 +0200

    Added new Spotify scopes

--- src/Skybrud.Social.Spotify/Scopes/SpotifyScopes.cs
+++ src/Skybrud.Social.Spotify/Scopes/SpotifyScopes.cs
@@ -1,13 +1,18 @@
 ﻿namespace Skybrud.Social.Spotify.Scopes {
-    
+
     /// <summary>
     /// Static class with properties representing scopes of available for the Spotify Web API.
     /// </summary>
     /// <see>
-    ///     <cref>https://developer.spotify.com/web-api/using-scopes/#list-of-scopes</cref>
+    ///     <cref>https://developer.spotify.com/documentation/general/guides/scopes/</cref>
     /// </see>
     public class SpotifyScopes {
-        
+
+        /// <summary>
+        /// Remote control playback of Spotify. This scope is currently available to Spotify iOS and Android App Remote SDKs.
+        /// </summary>
+        public static readonly SpotifyScope AppRemoteControl = new SpotifyScope("app-remote-control", "Remote control playback of Spotify. This scope is currently available to Spotify iOS and Android App Remote SDKs.");
+
         /// <summary>
         /// Read access to user's private playlists.
         /// </summary>
@@ -34,6 +39,11 @@
         public static readonly SpotifyScope Streaming = new SpotifyScope("streaming	", "Control playback of a Spotify track. This scope is currently only available to Spotify native SDKs (for example, the iOS SDK and the Android SDK). The user must have a Spotify Premium account.");
 
         /// <summary>
+        /// Read access to a user’s currently playing track.
+        /// </summary>
+        public static readonly SpotifyScope UserReadCurrentlyPlaying = new SpotifyScope("user-read-currently-playing", "Read access to a user’s currently playing track.");
+
+        /// <summary>
         /// Write/delete access to the list of artists and other users that the user follows.
         /// </summary>
         public static readonly SpotifyScope UserFollowModify = new SpotifyScope("user-follow-modify", "Write/delete access to the list of artists and other users that the user follows.");
@@ -54,9 +64,24 @@
         public static readonly SpotifyScope UserLibraryModify = new SpotifyScope("user-library-modify", "Write/delete access to a user's \"Your Music\" library.");
 
         /// <summary>
+        /// Write access to a user’s playback state.
+        /// </summary>
+        public static readonly SpotifyScope UserModifyPlaybackState = new SpotifyScope("user-modify-playback-state", "Write access to a user’s playback state.");
+
+        /// <summary>
+        /// Read access to a user’s player state.
+        /// </summary>
+        public static readonly SpotifyScope UserReadPlaybackState = new SpotifyScope("user-read-playback-state", "Read access to a user’s player state.");
+
+        /// <summary>
         /// Read access to user’s subscription details (type of user account).
         /// </summary>
         public static readonly SpotifyScope UserReadPrivate = new SpotifyScope("user-read-private", "Read access to user’s subscription details (type of user account).");
+
+        /// <summary>
+        /// 	Read access to a user’s recently played tracks.
+        /// </summary>
+        public static readonly SpotifyScope UserReadRecentlyPlayed = new SpotifyScope("user-read-recently-played", "Read access to a user’s recently played tracks.");
 
         /// <summary>
         /// Read access to the user's birthdate.
@@ -67,6 +92,11 @@
         /// Read access to user’s email address.
         /// </summary>
         public static readonly SpotifyScope UserReadEmail = new SpotifyScope("user-read-email", "Read access to user’s email address.");
+
+        /// <summary>
+        /// Read access to a user's top artists and tracks.
+        /// </summary>
+        public static readonly SpotifyScope UserTopRead = new SpotifyScope("user-top-read", "Read access to a user's top artists and tracks.");
 
     }
 
